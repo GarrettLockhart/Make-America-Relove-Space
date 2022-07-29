6d2b92248df9ad287b84ebf8506ff8e360ce14e4
IsaacT <isaact@Isaacs-MacBook-Pro.local>
2022-07-28 18:52:32 -0600

    API2.0

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,11 +9,11 @@ $(document).ready(function () {
   var API_KEY = "pHcVaecY6RB0BWtz3VukErxnlV5Zdw6aZ5thATLI";
 
   var API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
-// this is a const that creates a dynamic date with iso format and slice.
+  // this is a const that creates a dynamic date with iso format and slice.
   const isoDate = new Date().toISOString().slice(0, 10);
-//   this is the api and the where parameters, used template strings back ticks
-  const photoApi = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1&page=1&earth_date=${isoDate}&api_key=${API_KEY}`;
-//   this is console log for dynamic date
+  //   this is the api and the where parameters, used template strings back ticks
+  const photoApi = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&page=1&earth_date=${isoDate}&api_key=${API_KEY}`;
+  //   this is console log for dynamic date
   console.log(isoDate);
   fetch(API_URL).then(function (response) {
     if (response.ok) {
@@ -26,16 +26,23 @@ $(document).ready(function () {
       console.log("there was an error");
     }
   });
-// this is fetching and function thats appendng the images in the dom.
+  // this is fetching and function thats appendng the images in the dom.
   fetch(photoApi).then(function (response) {
     if (response.ok) {
       console.log(response);
       response.json().then(function (data) {
-        data.photos.forEach(appendImg);
-        // console.log(data);
-        // data.photos.forEach(item => {
-        //     console.log(item.img_src);
-        // });
+        data.photos.forEach((photo) => {
+          const divs = document.createElement("div");
+          let classes = `w-1/2 p-1 md:p-2`.split(" ");
+          divs.classList.add(...classes);
+          document.getElementById("images").appendChild(divs);
+          divs.appendChild(
+            element("img", {
+              src: photo.img_src,
+              alt: photo.id,
+            })
+          );
+        });
       });
     } else {
       console.log("there was an error");
@@ -43,17 +50,6 @@ $(document).ready(function () {
   });
   //   this is an arrow funtion
   const element = (tag, attributes = {}) =>
-//   this is an object assign
+    //   this is an object assign
     Object.assign(document.createElement(tag), attributes);
-  const appendImg = function (item) {
-    document.getElementById("images").appendChild(
-      element("img", {
-        src: item.img_src,
-        alt: item.id,
-        width: "100",
-        height: "100",
-        // add/change attributes to size imgages either thru js or tailwind. object.assign
-      })
-    );
-  };
 });
