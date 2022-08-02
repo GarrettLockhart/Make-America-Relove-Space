b3990361ede001f261936d601fceaae9a3ac88cc
IsaacT <isaact@Isaacs-MacBook-Pro.local>
2022-08-01 18:34:35 -0600

    final api commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,15 +4,12 @@ $("#light-mode-toggle").on("click", function () {
     $(iconEL).toggleClass("fa-solid");
   }
 });
+const API_KEY = "pHcVaecY6RB0BWtz3VukErxnlV5Zdw6aZ5thATLI";
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+const isoDate = new Date().toISOString().slice(0, 10);
 
 $(document).ready(function () {
-  var API_KEY = "pHcVaecY6RB0BWtz3VukErxnlV5Zdw6aZ5thATLI";
-
-  var API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
   // this is a const that creates a dynamic date with iso format and slice.
-  const isoDate = new Date().toISOString().slice(0, 10);
-  //   this is the api and the where parameters, used template strings back ticks
-  const photoApi = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&page=1&earth_date=${isoDate}&api_key=${API_KEY}`;
   //   this is console log for dynamic date
   console.log(isoDate);
   fetch(API_URL).then(function (response) {
@@ -27,22 +24,37 @@ $(document).ready(function () {
     }
   });
   // this is fetching and function thats appendng the images in the dom.
+});
+
+function search() {
+  //set document.getid to variable
+  const searchText = document.getElementById("parameters").value;
+  //   this is the api and the where parameters, used template strings back ticks
+  const photoApi = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${searchText}&page=1&earth_date=${isoDate}&api_key=${API_KEY}`;
+  const imagesDiv = document.getElementById("images");
+  clearSearchRestuls();
   fetch(photoApi).then(function (response) {
     if (response.ok) {
-      console.log(response);
       response.json().then(function (data) {
-        data.photos.forEach((photo) => {
-          const divs = document.createElement("div");
-          let classes = `w-1/2 p-1 md:p-2`.split(" ");
-          divs.classList.add(...classes);
-          document.getElementById("images").appendChild(divs);
-          divs.appendChild(
-            element("img", {
-              src: photo.img_src,
-              alt: photo.id,
-            })
-          );
-        });
+        if (data.photos.length > 0) {
+          data.photos.forEach((photo) => {
+            const divs = document.createElement("div");
+            let classes = `w-1/2 p-1 md:p-2`.split(" ");
+            divs.classList.add(...classes);
+            imagesDiv.appendChild(divs);
+            divs.appendChild(
+              element("img", {
+                src: photo.img_src,
+                alt: photo.id,
+              })
+            );
+          });
+        } else {
+          const pTag = document.createElement("pTag");
+          pTag.className = "text-white";
+          imagesDiv.appendChild(pTag);
+          pTag.innerText = "There were no photos taken on this sol date";
+        }
       });
     } else {
       console.log("there was an error");
@@ -52,4 +64,8 @@ $(document).ready(function () {
   const element = (tag, attributes = {}) =>
     //   this is an object assign
     Object.assign(document.createElement(tag), attributes);
-});
+}
+
+function clearSearchRestuls() {
+  document.getElementById("images").innerHTML = "";
+}
